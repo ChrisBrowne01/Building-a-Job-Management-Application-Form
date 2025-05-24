ae019133cba8babae58a8173f254e1bd4ffcc451
Christina Browne <112651196+ChrisBrowne01@users.noreply.github.com>
2025-05-24 23:35:55 +0100

    Update footer text to reflect correct application name

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Footer = () => {
   return (
     <footer>
-      <h2>Job Board was created by Christina Browne</h2>
+      <h2>Job Management was created by Christina Browne</h2>
       <p>2025 &copy;. All rights reserved.</p>
     </footer>
   )
